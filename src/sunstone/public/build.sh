1cf4031255b044ead92ac384aa60f65df0faa2c0
Jan Orel <jorel@opennebula.systems>
2020-01-06 12:24:06 +0100

    Revert "debug bower"
    This reverts commit adf52b120db9874d27e6196420fcff1b39101211.

--- src/sunstone/public/build.sh
+++ src/sunstone/public/build.sh
@@ -31,7 +31,7 @@ install_patch() {
 
     bower install --force --allow-root --config.interactive=false
 
-    (cd bower_components/no-vnc/ && npm install && ./utils/use_require.js --clean --as amd && sed -i -e "s/'\.\//'\.\.\/bower_components\/no-vnc\/lib\//g" lib/rfb.js ) || true
+    (cd bower_components/no-vnc/ && npm install && ./utils/use_require.js --clean --as amd && sed -i -e "s/'\.\//'\.\.\/bower_components\/no-vnc\/lib\//g" lib/rfb.js )
 
     PATCH_DIR="./patches/"
 
