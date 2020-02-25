a0967a85ba1c87bc36c7745eb0eb410a9c22f7e2
Jan Orel <jorel@opennebula.systems>
2020-02-25 15:34:16 +0100

    B #4236: Make PyONE threadsafe (prototype)
    * replace urlib2 by requests
    * based on bit.ly/2w76EhS
    * pass https option

--- src/oca/python/pyone/__init__.py
+++ src/oca/python/pyone/__init__.py
@@ -21,6 +21,13 @@ import socket
 from .util import cast2one
 from .acl import OneAcl
 
+import requests
+import requests.utils
+
+import sys
+from distutils.version import StrictVersion
+import warnings
+
 #
 # Exceptions as defined in the XML-API reference
 #
@@ -166,7 +173,18 @@ class OneServer(xmlrpc.client.ServerProxy):
         self.__helpers = {
             "marketapp.export": marketapp_export
         }
-        xmlrpc.client.ServerProxy.__init__(self, uri, **options)
+
+        if uri.startswith('https'):
+            RequestsTransport.use_http = True
+
+        transport = RequestsTransport()
+        transport.set_https(uri.startswith('https'))
+
+        xmlrpc.client.ServerProxy.__init__(
+                self,
+                uri,
+                transport=transport,
+                **options)
 
     #
     def _ServerProxy__request(self, methodname, params):
@@ -253,4 +271,80 @@ class OneServer(xmlrpc.client.ServerProxy):
         pass
 
 
+class RequestsTransport(xmlrpc.client.Transport):
+    """
+    Drop in Transport for xmlrpclib that uses Requests instead of httplib
+    """
 
+    user_agent = "Python XMLRPC with Requests (python-requests.org)"
+    use_https = False
+
+    def set_https(self, https=False):
+        self.use_https = https
+
+    def request(self, host, handler, request_body, verbose, **kwargs):
+        """
+        Make an xmlrpc request.
+        """
+        headers = {'User-Agent': self.user_agent,
+                   'Content-Type': 'text/xml',
+                   'Accept': '*/*'
+                   }
+
+        handler = handler.lstrip('/')
+
+        url = self._build_url(host, handler)
+        kwargs = {}
+        if StrictVersion(requests.__version__) >= StrictVersion('0.8.8'):
+            kwargs['verify'] = True
+        else:
+            if self.use_https:
+                warnings.warn('using https transport but no certificate '
+                              'verification.')
+        try:
+            resp = requests.post(url, data=request_body, headers=headers,
+                                 **kwargs)
+        except ValueError:
+            raise
+        except Exception:
+            raise  # something went wrong
+        else:
+            try:
+                resp.raise_for_status()
+            except requests.RequestException as e:
+                raise xmlrpc.client.ProtocolError(url, resp.status_code,
+                                                  str(e), resp.headers)
+            else:
+                return self.parse_response(resp)
+
+    def parse_response(self, resp):
+        """
+        Parse the xmlrpc response.
+        """
+        p, u = self.getparser()
+
+        if hasattr(resp, 'text'):
+            text = resp.text  # this is unicode(py2)/str(py3)
+        else:
+
+            encoding = requests.utils.get_encoding_from_headers(resp.headers)
+            if encoding is None:
+                encoding = 'utf-8'
+
+            if sys.version_info[0] == 2:
+                text = unicode(resp.content, encoding, errors='replace')
+            else:
+                assert sys.version_info[0] == 3
+                text = str(resp.content, encoding, errors='replace')
+        p.feed(text)
+        p.close()
+        return u.close()
+
+    def _build_url(self, host, handler):
+        """
+        Build a url for our request based on the host, handler and use_http
+        property
+        """
+        scheme = 'https' if self.use_https else 'http'
+
+        return '%s://%s/%s' % (scheme, host, handler)
